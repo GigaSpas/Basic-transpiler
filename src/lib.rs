0b593634373f5db1c32e027fc34e3a24732b4bd9
GigaSpas <GigaSpas@proton.me>
2024-03-26 21:06:35 +0200

    Added parsing for For statements

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,13 @@ use std::error::Error;
 ///////////////
 
 #[derive(Debug, Clone)]
-pub enum Bracket{
+pub enum Bracket {
     Curly,
     Square,
     Paren,
 }
 #[derive(Debug, Clone, Copy)]
-pub enum Symbol{
+pub enum Symbol {
     Equals,
     Plus,
     Minus,
@@ -27,7 +27,7 @@ pub enum Symbol{
     Exclamation,
 }
 #[derive(Debug, Clone)]
-pub enum Token{
+pub enum Token {
     BracketClose(Bracket),
     BracketOpen(Bracket),
     Number(String),
@@ -36,7 +36,7 @@ pub enum Token{
     Symbol(Symbol),
 }
 
-pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
+pub fn tokenise(input: String) -> Result<Vec<Token>, String> {
     let mut tokens: Vec<Token> = Vec::new();
     let chars: Vec<char> = input.chars().collect();
     let mut i = 0;
@@ -44,7 +44,7 @@ pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
     while i < chars.len() {
         let mut curr_char = chars[i];
 
-        if is_whitespace(&curr_char.to_string()){
+        if is_whitespace(&curr_char.to_string()) {
             i += 1;
             continue;
         }
@@ -56,9 +56,9 @@ pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
             curr_char = chars[i];
 
             while is_letter(&curr_char.to_string()) || is_number(&curr_char.to_string()) {
-            char.push(curr_char);
-            i += 1;
-            curr_char = chars[i];
+                char.push(curr_char);
+                i += 1;
+                curr_char = chars[i];
             }
 
             tokens.push(Token::Char(char));
@@ -71,9 +71,9 @@ pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
             curr_char = chars[i];
 
             while is_number(&curr_char.to_string()) {
-            num.push(curr_char);
-            i += 1;
-            curr_char = chars[i];
+                num.push(curr_char);
+                i += 1;
+                curr_char = chars[i];
             }
 
             tokens.push(Token::Number(num));
@@ -81,24 +81,24 @@ pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
         }
 
         if curr_char == '\"' {
-            let mut str = String::new(); 
+            let mut str = String::new();
             i += 1;
             curr_char = chars[i];
 
             while curr_char != '\"' {
-            //Deal with escaped chars
-            if curr_char == '\\'{
-            i += 1;
-            curr_char = chars[i];
-            str.push(curr_char);
-            continue;
+                //Deal with escaped chars
+                if curr_char == '\\' {
+                    i += 1;
+                    curr_char = chars[i];
+                    str.push(curr_char);
+                    continue;
+                }
+
+                str.push(curr_char);
+                i += 1;
+                curr_char = chars[i];
             }
 
-            str.push(curr_char);
-            i += 1;
-            curr_char = chars[i];
-            }
-            
             i += 1;
             tokens.push(Token::String(str));
             continue;
@@ -106,25 +106,91 @@ pub fn tokenise(input: String) -> Result<Vec<Token>, String>{
 
         //Deal with single characters
         match curr_char {
-
-            '=' => {tokens.push(Token::Symbol(Symbol::Equals)); i += 1; continue},
-            '-' => {tokens.push(Token::Symbol(Symbol::Minus)); i += 1; continue},
-            '+' => {tokens.push(Token::Symbol(Symbol::Plus)); i += 1; continue},
-            '*' => {tokens.push(Token::Symbol(Symbol::Multiply)); i += 1; continue},
-            '/' => {tokens.push(Token::Symbol(Symbol::Devide)); i += 1; continue},
-            '%' => {tokens.push(Token::Symbol(Symbol::Modulo)); i += 1; continue},
-            '>' => {tokens.push(Token::Symbol(Symbol::Grater)); i += 1; continue},
-            '<' => {tokens.push(Token::Symbol(Symbol::Lesser)); i += 1; continue},
-            '.' => {tokens.push(Token::Symbol(Symbol::Dot)); i += 1; continue},
-            ';' => {tokens.push(Token::Symbol(Symbol::SemiColon)); i += 1; continue},
-            '!' => {tokens.push(Token::Symbol(Symbol::Exclamation)); i += 1; continue},
-            '{' => {tokens.push(Token::BracketOpen(Bracket::Curly)); i += 1; continue},
-            '}' => {tokens.push(Token::BracketClose(Bracket::Curly)); i += 1; continue},
-            '(' => {tokens.push(Token::BracketOpen(Bracket::Paren)); i += 1; continue},
-            ')' => {tokens.push(Token::BracketClose(Bracket::Paren)); i += 1; continue},
-            '[' => {tokens.push(Token::BracketOpen(Bracket::Square)); i += 1; continue},
-            ']' => {tokens.push(Token::BracketClose(Bracket::Square)); i += 1; continue},
-
+            '=' => {
+                tokens.push(Token::Symbol(Symbol::Equals));
+                i += 1;
+                continue;
+            }
+            '-' => {
+                tokens.push(Token::Symbol(Symbol::Minus));
+                i += 1;
+                continue;
+            }
+            '+' => {
+                tokens.push(Token::Symbol(Symbol::Plus));
+                i += 1;
+                continue;
+            }
+            '*' => {
+                tokens.push(Token::Symbol(Symbol::Multiply));
+                i += 1;
+                continue;
+            }
+            '/' => {
+                tokens.push(Token::Symbol(Symbol::Devide));
+                i += 1;
+                continue;
+            }
+            '%' => {
+                tokens.push(Token::Symbol(Symbol::Modulo));
+                i += 1;
+                continue;
+            }
+            '>' => {
+                tokens.push(Token::Symbol(Symbol::Grater));
+                i += 1;
+                continue;
+            }
+            '<' => {
+                tokens.push(Token::Symbol(Symbol::Lesser));
+                i += 1;
+                continue;
+            }
+            '.' => {
+                tokens.push(Token::Symbol(Symbol::Dot));
+                i += 1;
+                continue;
+            }
+            ';' => {
+                tokens.push(Token::Symbol(Symbol::SemiColon));
+                i += 1;
+                continue;
+            }
+            '!' => {
+                tokens.push(Token::Symbol(Symbol::Exclamation));
+                i += 1;
+                continue;
+            }
+            '{' => {
+                tokens.push(Token::BracketOpen(Bracket::Curly));
+                i += 1;
+                continue;
+            }
+            '}' => {
+                tokens.push(Token::BracketClose(Bracket::Curly));
+                i += 1;
+                continue;
+            }
+            '(' => {
+                tokens.push(Token::BracketOpen(Bracket::Paren));
+                i += 1;
+                continue;
+            }
+            ')' => {
+                tokens.push(Token::BracketClose(Bracket::Paren));
+                i += 1;
+                continue;
+            }
+            '[' => {
+                tokens.push(Token::BracketOpen(Bracket::Square));
+                i += 1;
+                continue;
+            }
+            ']' => {
+                tokens.push(Token::BracketClose(Bracket::Square));
+                i += 1;
+                continue;
+            }
 
             _ => return Err(format!("Tokeniser: Invalid character: {}", curr_char)),
         }
@@ -161,14 +227,37 @@ pub enum Node {
     StringLiteral(String),
     NumberLiteral(String),
     Symbol(Symbol),
-    Veriable{name: String, value: String},
+    Veriable {
+        name: String,
+        value: String,
+    },
     VeriableCall(String),
-    IfStatement{condition: Vec<Node>, body: Vec<Node>},
-    ElseStatement{body: Vec<Node>},
-    ForLoop{var_name: String, start: String, end: String, body: Vec<Node>},
-    WhileLoop{condition: Vec<Node>, body: Vec<Node>},
-    Function{name: String, input: Vec<Node>, body: Vec<Node>},
-    FunctionCall{name: String, input: Vec<Node>},
+    IfStatement {
+        condition: Vec<Node>,
+        body: Vec<Node>,
+    },
+    ElseStatement {
+        body: Vec<Node>,
+    },
+    ForLoop {
+        var_name: String,
+        start: String,
+        end: String,
+        body: Vec<Node>,
+    },
+    WhileLoop {
+        condition: Vec<Node>,
+        body: Vec<Node>,
+    },
+    Function {
+        name: String,
+        input: Vec<Node>,
+        body: Vec<Node>,
+    },
+    FunctionCall {
+        name: String,
+        input: Vec<Node>,
+    },
 }
 
 pub struct Output {
@@ -176,7 +265,7 @@ pub struct Output {
     end_num: usize,
 }
 
-pub fn parser(start: usize, tokens: Vec<Token>) -> Result<Output, Box<dyn Error>>{
+pub fn parser(start: usize, tokens: Vec<Token>) -> Result<Output, Box<dyn Error>> {
     let mut ast: Vec<Node> = Vec::new();
     let mut i = start;
 
@@ -184,16 +273,16 @@ pub fn parser(start: usize, tokens: Vec<Token>) -> Result<Output, Box<dyn Error>
         let mut curr_token = &tokens[i];
 
         if let Token::Char(a) = curr_token {
-
-            if a == "let"{
+            if a == "let" {
                 let name;
                 let value;
                 i += 1;
                 curr_token = &tokens[i];
                 if let Token::Char(b) = curr_token {
                     name = b.to_string();
+                } else {
+                    return Err(format!("Parser: Expected char got: {:?}", curr_token).into());
                 }
-                else{ return Err(format!("Parser: Expected char got: {:?}",curr_token).into())}
 
                 i += 2;
                 curr_token = &tokens[i];
@@ -202,92 +291,189 @@ pub fn parser(start: usize, tokens: Vec<Token>) -> Result<Output, Box<dyn Error>
                     Token::Char(c) => value = c.to_string(),
                     Token::Number(n) => value = n.to_string(),
                     Token::String(s) => value = s.to_string(),
-                    _ => return Err(format!("Parser: Expected Char/String/Number got: {:?}", curr_token).into())
+                    _ => {
+                        return Err(format!(
+                            "Parser: Expected Char/String/Number got: {:?}",
+                            curr_token
+                        )
+                        .into())
+                    }
                 }
                 i += 1;
-                ast.push(Node::Veriable{name, value});
+                ast.push(Node::Veriable { name, value });
                 continue;
-            }    
+            }
 
-            if a == "if"{
+            if a == "if" {
                 // Create the condition
-                let mut condition: Vec<Node>= vec!();
+                let mut condition: Vec<Node> = vec![];
                 i += 1;
                 curr_token = &tokens[i];
 
-                while !matches!(*curr_token, Token::BracketOpen(Bracket::Curly)){
-                    match curr_token{
+                while !matches!(*curr_token, Token::BracketOpen(Bracket::Curly)) {
+                    match curr_token {
                         Token::Number(n) => condition.push(Node::NumberLiteral(n.to_string())),
                         Token::String(s) => condition.push(Node::StringLiteral(s.to_string())),
                         Token::Char(c) => condition.push(Node::VeriableCall(c.to_string())),
                         Token::Symbol(s) => condition.push(Node::Symbol(*s)),
-                        _ => return Err(format!("Parser: Expected Char/String/Number got: {:?}", curr_token).into())
+                        _ => {
+                            return Err(format!(
+                                "Parser: Expected Char/String/Number got: {:?}",
+                                curr_token
+                            )
+                            .into())
+                        }
                     }
                     i += 1;
                     curr_token = &tokens[i];
                 }
                 i += 1;
                 // Creat the body of the statement
-                let result= parser(i, tokens.to_vec())?;
-                match result.node{
-                    Node::Program(body) => ast.push(Node::IfStatement{condition, body}),
-                    _ => return Err("Error".into())
+                let result = parser(i, tokens.to_vec())?;
+                match result.node {
+                    Node::Program(body) => ast.push(Node::IfStatement { condition, body }),
+                    _ => return Err("Error".into()),
                 }
                 i = result.end_num + 1;
 
                 continue;
             }
 
-            if a == "else"{
+            if a == "else" {
                 i += 2;
                 // Creat the body of the statement
-                let result= parser(i, tokens.to_vec())?;
-                match result.node{
-                    Node::Program(body) => ast.push(Node::ElseStatement{body}),
-                    _ => return Err("Error".into())
+                let result = parser(i, tokens.to_vec())?;
+                match result.node {
+                    Node::Program(body) => ast.push(Node::ElseStatement { body }),
+                    _ => return Err("Error".into()),
                 }
                 i = result.end_num + 1;
 
                 continue;
             }
 
-            if a == "while"{
+            if a == "while" {
                 // Create the condition
-                let mut condition: Vec<Node>= vec!();
+                let mut condition: Vec<Node> = vec![];
                 i += 1;
                 curr_token = &tokens[i];
 
-                while !matches!(*curr_token, Token::BracketOpen(Bracket::Curly)){
-                    match curr_token{
+                while !matches!(*curr_token, Token::BracketOpen(Bracket::Curly)) {
+                    match curr_token {
                         Token::Number(n) => condition.push(Node::NumberLiteral(n.to_string())),
                         Token::String(s) => condition.push(Node::StringLiteral(s.to_string())),
                         Token::Char(c) => condition.push(Node::VeriableCall(c.to_string())),
                         Token::Symbol(s) => condition.push(Node::Symbol(*s)),
-                        _ => return Err(format!("Parser: Expected Char/String/Number got: {:?}", curr_token).into())
+                        _ => {
+                            return Err(format!(
+                                "Parser: Expected Char/String/Number got: {:?}",
+                                curr_token
+                            )
+                            .into())
+                        }
                     }
                     i += 1;
                     curr_token = &tokens[i];
                 }
                 i += 1;
                 // Creat the body of the statement
-                let result= parser(i, tokens.to_vec())?;
-                match result.node{
-                    Node::Program(body) => ast.push(Node::IfStatement{condition, body}),
-                    _ => return Err("Error".into())
+                let result = parser(i, tokens.to_vec())?;
+                match result.node {
+                    Node::Program(body) => ast.push(Node::WhileLoop { condition, body }),
+                    _ => return Err("Error".into()),
                 }
                 i = result.end_num + 1;
 
                 continue;
             }
 
+            if a == "for" {
+                // Create the condition
+                i += 1;
+                curr_token = &tokens[i];
+                let var_name;
+                let start;
+                if let Token::Char(c) = curr_token {
+                    var_name = c.to_string();
+                } else {
+                    return Err(format!("Parser: Expected Char got: {:?}", curr_token).into());
+                }
 
+                i += 2;
+                curr_token = &tokens[i];
 
-        }
-            match curr_token {
-                Token::BracketClose(_) => return Ok(Output{node: Node::Program(ast), end_num: i}),
-                _ => return Err(format!("Error {:?} --- {:?}", ast, curr_token).into())
+                match curr_token {
+                    Token::Number(n) => start = n.to_string(),
+                    Token::Char(c) => start = c.to_string(),
+                    _ => {
+                        return Err(
+                            format!("Parser: Expected Num/Char got: {:?}", curr_token).into()
+                        )
+                    }
+                }
+
+                i += 3;
+                curr_token = &tokens[i];
+                let end;
+
+                if let Token::Symbol(_) = curr_token {
+                    i += 1;
+                    curr_token = &tokens[i];
+
+                    match curr_token {
+                        Token::Number(n) => end = (n.parse::<usize>()? - 1).to_string(),
+                        Token::Char(c) => end = (c.parse::<usize>()? - 1).to_string(),
+                        _ => {
+                            return Err(
+                                format!("Parser: Expected Num/Char got: {:?}", curr_token).into()
+                            )
+                        }
+                    }
+                } else if let Token::Number(_) = curr_token {
+                    match curr_token {
+                        Token::Number(n) => end = n.to_string(),
+                        Token::Char(c) => end = c.to_string(),
+                        _ => {
+                            return Err(
+                                format!("Parser: Expected Num/Char got: {:?}", curr_token).into()
+                            )
+                        }
+                    }
+                } else {
+                    return Err(format!("Parser: Expected Char got: {:?}", curr_token).into());
+                }
+
+                i += 2;
+                // Creat the body of the statement
+                let result = parser(i, tokens.to_vec())?;
+                match result.node {
+                    Node::Program(body) => ast.push(Node::ForLoop {
+                        var_name,
+                        start,
+                        end,
+                        body,
+                    }),
+                    _ => {
+                        return Err(format!("Parser: Could not parse body: {:?}", curr_token).into())
+                    }
+                }
+                i = result.end_num + 1;
+
+                continue;
             }
+        }
+        match curr_token {
+            Token::BracketClose(_) => {
+                return Ok(Output {
+                    node: Node::Program(ast),
+                    end_num: i,
+                })
+            }
+            _ => return Err(format!("Parser: Could not parse: {:?}", curr_token).into()),
+        }
     }
-    Ok(Output{node: Node::Program(ast), end_num: i})
+    Ok(Output {
+        node: Node::Program(ast),
+        end_num: i,
+    })
 }
-
